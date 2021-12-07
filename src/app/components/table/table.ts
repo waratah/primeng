863e266d5e99e1653adf0b300d5f2ef0f44409e0
Ken Foskey <ken.foskey@bluestone.com.au>
2021-12-07 23:06:01 +1100

    :bug: 10903.  width not coded on data element of table

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -2343,7 +2343,7 @@ export class Table implements OnInit, AfterViewInit, AfterContentInit, Blockable
             if (this.columnResizeMode === 'expand' && this.tableWidthState) {
                 this.tableViewChild.nativeElement.style.width = this.tableWidthState;
                 this.tableViewChild.nativeElement.style.minWidth = this.tableWidthState;
-                this.containerViewChild.nativeElement.style.width = this.tableWidthState;
+                this.wrapperViewChild.nativeElement.style.width = this.tableWidthState;
             }
 
             this.createStyleElement();
